a2c6320165a98d219ef0205e14ca4213d2eaf7f0
CandraJengger <cakjengger@gmail.com>
2020-04-13 01:35:16 +0700

    change sidebar to responsive

--- js/script.js
+++ js/script.js
@@ -25,15 +25,35 @@ arrow.addEventListener('click', function(e) {
 //Sidebar
 const tabSidebar = document.getElementById('stream')
 const sidebar = document.querySelector('.sidebar')
+const tabOpen = document.querySelector('header .tab-open')
+const navbar = document.querySelector('.navbar')
+const main = document.querySelector('main')
+const hero = document.querySelector('main #content .hero')
 
 tabSidebar.addEventListener('click',function() {
     sidebar.style.right = '-100px'
+    tabOpen.style.display = 'inline-block'
+    navbar.style.width = '100%'
+    main.style.width = '100%'
+    footer.style.width = '100%'
+    hero.style.backgroundPositionY = '-540px'
+    hero.style.backgroundPositionX = '100px'
+})
+
+tabOpen.addEventListener('click', function() {
+    sidebar.style.right = '0'
+    tabOpen.style.display = 'none'
+    navbar.style.width = '94%'
+    main.style.width = '94%'
+    footer.style.width = '94%'
+    hero.style.backgroundPositionY = '-570px'
+    hero.style.backgroundPositionX = '0'
 })
 
 window.addEventListener('scroll',function(e) {
     if(window.scrollY > 30) {
-        document.querySelector('.navbar').classList.add('white')
+        document.querySelector('.navbar').classList.add('green')
     } else {
-        document.querySelector('.navbar').classList.remove('white')
+        document.querySelector('.navbar').classList.remove('green')
     }
 })
