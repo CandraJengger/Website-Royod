a7743949a7692b25306a8e99a7952f8633e516a3
CandraJengger <cakjengger@gmail.com>
2020-04-13 11:02:38 +0700

    make resposive to desktop and fixed search icon

--- js/script.js
+++ js/script.js
@@ -36,8 +36,20 @@ tabSidebar.addEventListener('click',function() {
     navbar.style.width = '100%'
     main.style.width = '100%'
     footer.style.width = '100%'
-    hero.style.backgroundPositionY = '-540px'
-    hero.style.backgroundPositionX = '100px'
+    if(window.innerWidth > 1198) {
+        hero.style.backgroundPositionY = '-540px'
+        hero.style.backgroundPositionX = '100px'
+    } else if(window.innerWidth < 1198 && window.innerWidth > 1100) {
+        hero.style.backgroundPositionY = '-540px'
+        hero.style.backgroundPositionX = '50px'
+    } else if(window.innerWidth < 1100 && window.innerWidth > 835) {
+        hero.style.backgroundPositionY = '-360px'
+        hero.style.backgroundPositionX = '-20px'
+    }
+
+
+   HideSearch()
+    
 })
 
 tabOpen.addEventListener('click', function() {
@@ -46,9 +58,42 @@ tabOpen.addEventListener('click', function() {
     navbar.style.width = '94%'
     main.style.width = '94%'
     footer.style.width = '94%'
-    hero.style.backgroundPositionY = '-570px'
-    hero.style.backgroundPositionX = '0'
+    if(window.innerWidth > 1198) {
+        hero.style.backgroundPositionY = '-570px'
+        hero.style.backgroundPositionX = '0'
+    } else if(window.innerWidth < 1198 && window.innerWidth > 1100) {
+        hero.style.backgroundPositionY = '-570px'
+        hero.style.backgroundPositionX = '-150px'
+    } else if(window.innerWidth < 1198 && window.innerWidth > 835) {
+        hero.style.backgroundPositionY = '-400px'
+        hero.style.backgroundPositionX = '-170px'
+    }
+    
 })
+
+const search = document.querySelector('.sidebar .sidebar-list .wrap i')
+search.addEventListener('click', function() {
+    document.querySelector('.sidebar .sidebar-list .wrap').style.paddingRight = '70px'
+    document.querySelector('.sidebar .sidebar-list .wrap input').style.float = 'right'
+    document.querySelector('.sidebar .sidebar-list .wrap input').style.opacity = '1'
+    search.style.transform = ('translateX(-30px)')
+    search.style.color = '#c3c3c37d'
+})    
+
+function HideSearch() {
+    document.querySelector('.sidebar .sidebar-list .wrap input').style.opacity = '0'
+    setTimeout(function() {
+        document.querySelector('.sidebar .sidebar-list .wrap input').style.float = 'left'
+        document.querySelector('.sidebar .sidebar-list .wrap').style.paddingRight = '29px'
+        search.style.transform = ('translateX(0)')
+        search.style.color = '#000'
+    },350)
+}
+
+main.firstElementChild.addEventListener('click', function() {
+    HideSearch()
+})
+
 
 window.addEventListener('scroll',function(e) {
     if(window.scrollY > 30) {
